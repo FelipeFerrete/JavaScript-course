4cb7af10ec86f2af36210c88cb8125f2eee8cc3c
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-02 12:57:50 -0300

    Funcoes com parametros

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do Numero Secreto";
+// let titulo = document.querySelector("h1");
+// titulo.innerHTML = "Jogo do Numero Secreto";
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um numero entre 0 e 10.";
+// let paragrafo = document.querySelector("p");
+// paragrafo.innerHTML = "Escolha um numero entre 0 e 10.";
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML =  texto;
+}
+
+exibirTextoNaTela("h1","Jogo do Numero Secreto");
+exibirTextoNaTela("p","Escolha um numero entre 0 e 100.")
 
 function verificarChute() {
-    console.log("Botao Clickado");
+    console.log("Botao Clickado")
 }
